1f1b8f6534bd2acd1ac2e3c56d96409c2a9bfb69
alaminshakil111 <alaminshakil111@gmail.com>
2025-02-17 23:01:39 +0600

    appstore

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,10 @@
 import mainSlider1 from "../assets/images/mainSlider1.jpg";
 import mainSlider2 from "../assets/images/mainSlider2.jpg";
 import mainSlider3 from "../assets/images/mainSlider3.jpg";
-import playstore-square from "../assets/images/playstore-square.png";
-import appstore-square from "../assets/images/appstore-square.png";
+import playstore_square from "../assets/images/playstore-square.png";
+import appstore_square from "../assets/images/appstore-square.png";
+import playstorebadge from "../assets/images/playstorebadge.svg";
+import fabrilifeappstore from "../assets/images/fabrilifeappstore.svg";
 
 
 const Home = () => {
@@ -22,23 +24,22 @@ const Home = () => {
                         <img src={mainSlider3} alt="mainSlider3" />
                     </div>
                     <div className="sliderCatagoryBtn">
-                        <div className="directbtnLeft">
+                        <div className="directbtnLeft flex justify-evenly">
                             <a href="/"> SHOP NOW </a>
                             <a href="/"> MEN </a>
                             <a href="/"> WOMEN </a>
                             <a href="/"> KIDS </a>
                         </div>
-                        <div className="directBtnRight">
+                        <div className="directBtnRight flex items-center">
                             <p> GET 5% OFF ON APP </p>
-                            <div className="discountAppForMobile">
-                                <img src= {playstore-square} alt="playstore-square" />
-                                <img src= {appstore-square} alt="appstore-square" />
+                            <div className="discountAppForMobile flex ">
+                                <img className="md:hidden" src= {playstore_square} alt="playstore-square" />
+                                <img className="md:hidden" src= {appstore_square} alt="appstore-square" />
                             </div>
-                            <div className="discountAppForDesktop">
-                                <img src= {playstore-square} alt="playstore-square" />
-                                <img src= {appstore-square} alt="appstore-square" />
+                            <div className="discountAppForDesktop flex ">
+                                <img className="hidden md:block" src= {playstorebadge} alt="playstorebadge" />
+                                <img className="hidden md:block" src= {fabrilifeappstore} alt="fabrilifeappstore" />
                             </div>
-
                         </div>
 
                     </div>
