2666260e1c7f62125ed41eef2b28318bb76619ba
alaminshakil111 <alaminshakil111@gmail.com>
2025-02-23 22:29:01 +0600

    element

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
                         <Link to="/" className="hover:text-white px-[22px] py-3 text-[11px] sm:text-[14px] w-full flex justify-center items-center text-center "> KIDS </Link>
 
                     </div>
-                    <div className="directBtnRight bg-[#ededed] mt-1 lg:mt-0 lg:w-[35%] flex justify-center  items-center  lg:py-2">
+                    <div className="directBtnRight bg-[#ededed] mt-1 mb-1 lg:mt-0 lg:mb-0 lg:w-[35%] flex justify-center  items-center  lg:py-2">
                         <div className=" flex justify-between items-center px-2 py-2 sm:py-4 lg:py-0 lg:px-0 max-w-[260px] sm:max-w-[415px] m-auto lg:m-0 lg:max-w-full">
                             <p className="text-xs md:text-[14px] px-2" > GET <span className="text-[16px] md:text-[17px] font-bold ">5%</span> OFF ON APP </p>
                             <div className="discountAppForMobile flex space-x-1 ">
@@ -40,21 +40,27 @@ const Home = () => {
                         </div>
                     </div>
                 </div>
-                <div className="homeElement bg-purple-400 ">
-                    <Link href="/">
-                        <div>
-                            <strong className="inline "> Event T-shirt <IoIosArrowForward /> </strong> 
-                            <span className="flex">T-shirt/Clothing with your brand logo or design? We are delivering worldwide at unbeatable prices. <strong> Click here  <FaArrowCircleRight /> </strong>  </span>
-                        </div>
-                      
-
-                    </Link>
-                </div>
-
-
-
                 </div>
             </div>
+            <div className="container px-2 sm:px-0">
+                <div className="wrapper">
+                    <div className="homeElement bg-purple-400 lg:mt-1">
+                        <Link href="/" className="px-2 py-2 block underline">
+                            <div className="homeElementInner ">
+                                <strong className="hover:text-blue-600"> Event T-shirt <IoIosArrowForward /> </strong> <span>
+                                T-shirt/Clothing with your brand logo or design? We are delivering worldwide at unbeatable prices. <strong className="hover:text-blue-600"> Click here  <FaArrowCircleRight /> </strong>
+                                </span>
+                            </div>
+                        </Link>
+                    </div>
+                    <div className="sectionHeading text-center bg-red-400 py-2 mt-1">
+                        <h3> NEW ARRIVAL </h3>
+                        <a href="/" className="underline text-center flex justify-center items-center text-"> visit store <IoIosArrowForward /> </a>
+                    </div>
+                </div>
+            </div>
+
+
         </>
         
     );
